1c87fccac626775c1f27f12532fadad7081a972f
simopucci <simo.pucci7@gmail.com>
2024-01-12 18:56:14 +0100

    feat: fix for loop

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -5,9 +5,19 @@ const mailInput = document.getElementById('user-mail');
 const mailArray = ['nomecognome@gmail.com', 'simonepucci@gmail.com', 'nicolagallo@gmail.com', 'jonathanlombardo@gmail.com', 'pippopluto@gmail.com', 'nome123@gmail.com'];
 
 buttonInput.addEventListener('click', function() {
-    mailInput.value;
+    const userMail = mailInput.value.trim().toLowerCase();
+    // console.log(userMail);
+    let isValid = false;
 
-    if (mailArray.includes(mailInput.value)) {
+    for (let i = 0; i < mailArray.length; i++) {
+        // console.log(mailArray[i]);
+
+        if (userMail == mailArray[i]) {
+          isValid = true;
+        }
+    }
+
+    if (isValid) {
         resultElement.innerHTML = 'Approvato';
         resultElement.classList.add('text-success');
         resultElement.classList.remove('text-danger');
@@ -18,3 +28,19 @@ buttonInput.addEventListener('click', function() {
         resultElement.classList.remove('text-success');
     }
 })
+
+
+// buttonInput.addEventListener('click', function() {
+//     mailInput.value;
+
+//     if (mailArray.includes(mailInput.value)) {
+//         resultElement.innerHTML = 'Approvato';
+//         resultElement.classList.add('text-success');
+//         resultElement.classList.remove('text-danger');
+
+//     } else {
+//         resultElement.innerHTML = 'Rifiutato';
+//         resultElement.classList.add('text-danger');
+//         resultElement.classList.remove('text-success');
+//     }
+// })
